085103fa1fc57d4b689c057d1b89f04c2bf4ed4e
Basil Eagle <basileagle@gmail.com>
2022-11-09 23:19:46 +0000

    fix so that it replaces file and use dot sourcing

--- backup-db.ps1
+++ backup-db.ps1
@@ -1,4 +1,6 @@
-$db_path = "C:\Users\Basil\data\basil.db"
+. ./env.ps1
 mega-login $env:MEGA_LOGIN $env:MEGA_PASSWORD
+mega-mv "sqlite_db/basil.db" "sqlite_db/basil2.db"
 mega-put $db_path "sqlite_db"
+mega-rm "sqlite_db/basil2.db"
 mega-logout
